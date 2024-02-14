376c557e14b6f4235874e76909b08074ceadecd8
Ayush7454 <ayushgaya7454@gmail.com>
2024-02-15 01:53:10 +0530

    changes

---
+++ page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import Landing from "./Home/Landing";
+
+
+const page = () => {
+    return(
+        <Landing />
+    )
+}
---
+++ src/Home.tsx/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='h-1 font-bold '>Landing</div>
+  )
+}
+
+export default Landing
